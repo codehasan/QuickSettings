467068415ff0c5ea4afcd22234394ae06afff521
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2026-01-02 13:40:35 +0600

    exclude dependency info from apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,16 +16,22 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    dependenciesInfo {
+        includeInBundle = false
+        includeInApk = false
     }
 }
 
