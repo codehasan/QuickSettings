3afceafa8724ace5bf3a946e073fe0ee9d53865e
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2026-01-14 11:46:28 +0600

    write settings helper methods

--- app/src/main/java/io/github/codehasan/quicksettings/services/common/BaseTileService.java
+++ app/src/main/java/io/github/codehasan/quicksettings/services/common/BaseTileService.java
@@ -2,11 +2,30 @@ package io.github.codehasan.quicksettings.services.common;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.provider.Settings;
 import android.service.quicksettings.TileService;
 
 public class BaseTileService extends TileService {
     public boolean hasSecureSettingsPermission() {
         return checkSelfPermission(Manifest.permission.WRITE_SECURE_SETTINGS)
                 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public boolean writeSecureSetting(String key, String value) {
+        return Settings.Secure.putString(getContentResolver(), key, value);
+    }
+
+    public String getSecureSetting(String key, String defaultValue) {
+        String value = Settings.Secure.getString(getContentResolver(), key);
+        return value == null ? defaultValue : value;
+    }
+
+    public boolean writeGlobalSetting(String key, String value) {
+        return Settings.Global.putString(getContentResolver(), key, value);
+    }
+
+    public String getGlobalSetting(String key, String defaultValue) {
+        String value = Settings.Global.getString(getContentResolver(), key);
+        return value == null ? defaultValue : value;
     }
 }
