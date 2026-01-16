5c4749031ee0625e679ec3d3160f010b8ebb65dd
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2026-01-16 08:55:18 +0600

    make volume tile stateless

--- app/src/main/java/io/github/codehasan/quicksettings/services/tile/VolumeService.java
+++ app/src/main/java/io/github/codehasan/quicksettings/services/tile/VolumeService.java
@@ -10,77 +10,23 @@
 
 package io.github.codehasan.quicksettings.services.tile;
 
-import static android.media.AudioManager.STREAM_MUSIC;
-import static java.lang.String.valueOf;
-
 import android.content.ActivityNotFoundException;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.os.Build;
 import android.provider.Settings;
-import android.service.quicksettings.Tile;
 
-import io.github.codehasan.quicksettings.services.common.StatefulTile;
+import io.github.codehasan.quicksettings.services.common.StatelessTile;
 import io.github.codehasan.quicksettings.util.TileServiceUtil;
 
-public class VolumeService extends StatefulTile {
+public class VolumeService extends StatelessTile {
     private AudioManager audioManager;
-
-    // Receiver to handle volume changes while the tile is visible
-    private final BroadcastReceiver volumeReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if ("android.media.VOLUME_CHANGED_ACTION".equals(intent.getAction())) {
-                updateTile();
-            }
-        }
-    };
 
     @Override
     public void onCreate() {
         super.onCreate();
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-    }
-
-    @Override
-    public void onStartListening() {
-        super.onStartListening();
-        // Register receiver to listen for external volume changes (e.g. physical buttons)
-        IntentFilter filter = new IntentFilter("android.media.VOLUME_CHANGED_ACTION");
-        registerReceiver(volumeReceiver, filter);
-    }
-
-    @Override
-    public void onStopListening() {
-        super.onStopListening();
-        // Unregister to save resources when tile is not visible
-        unregisterReceiver(volumeReceiver);
-    }
-
-    @Override
-    public void updateTile() {
-        if (audioManager == null) return;
-
-        Tile tile = getQsTile();
-        if (tile == null) return;
-
-        // Check if volume is fixed (cannot be changed)
-        if (audioManager.isVolumeFixed()) {
-            tile.setState(Tile.STATE_UNAVAILABLE);
-        } else {
-            int currentVolume = audioManager.getStreamVolume(STREAM_MUSIC);
-
-            // Active if volume > 0, Inactive if 0 (muted)
-            tile.setState(currentVolume > 0 ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                tile.setSubtitle(valueOf(currentVolume));
-            }
-        }
-
-        tile.updateTile();
     }
 
     @Override
